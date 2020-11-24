ac6f0232d29281217a81a795d2279acf848655eb
Luka M <50453817+luka-medic@users.noreply.github.com>
2020-11-24 02:14:27 +0100

    scripts: SMS generators

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,7 @@ function showDate(n) {
   }
 }
 
+
 function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
@@ -61,17 +62,121 @@ function changeTextS() {
 			text = text.concat(simpt_i+" ("+date_i+")");
 		}
 	}
-	text = text.concat(".<br>Poglej: https://covid-spark.info");
+	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
 	
 	splitArray = text.split("<br>");
 	
-	var finalString = "sms:?body='";
+	var finalString = "sms:?body=";
     for ( i = 0; i < splitArray.length; i++) {
         finalString += splitArray[i]+ "%0a"; 
         }
-	finalString += "'";
 	
 	document.getElementById('msgText').innerHTML=text;
+	document.getElementById('msgHref').href= finalString;
+	document.getElementById('msgHref2').href= finalString; 
+}
+
+function changeTextP() {
+	var text = 'Spark P, covid-spark.info<br>Mislim, da sva bila v zadnjem casu v stiku.<br>Sporocam ti, da imam pozitiven rezultat na SARS-CoV-2';
+	
+	flag = true;
+	for (i=0;i<simptoms.length;i++) {
+		var checkBox = document.getElementById("simpt".concat(i.toString()));
+        var simpt_i = simptoms_out[i].toLowerCase();
+		var date_i = formatDate(document.getElementById("date".concat(i.toString())).value);
+		
+		if (checkBox.checked == true){
+			if (flag){
+				text = text.concat(".<br>Simptomi, ki sem jih zaznal:<br>");
+				flag = false;
+			} else {
+			    text = text.concat(", ");
+			}
+			text = text.concat(simpt_i+" ("+date_i+")");
+		}
+	}
+	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
+	
+	splitArray = text.split("<br>");
+	
+	var finalString = "sms:?body=";
+    for ( i = 0; i < splitArray.length; i++) {
+        finalString += splitArray[i]+ "%0a"; 
+        }
+	
 	document.getElementById('msgText').innerHTML=text;
-	document.getElementById('msgHref').href= finalString; 
+	document.getElementById('msgHref').href= finalString;
+	document.getElementById('msgHref2').href= finalString; 
+}
+
+function changeTextN() {
+	var text = 'Spark N, covid-spark.info<br>Sporocam ti, da imam negativen rezultat na SARS-CoV-2.';
+	
+	splitArray = text.split("<br>");
+	
+	var finalString = "sms:?body=";
+    for ( i = 0; i < splitArray.length; i++) {
+        finalString += splitArray[i]+ "%0a"; 
+        }
+	
+	document.getElementById('msgText').innerHTML=text;
+	document.getElementById('msgHref').href= finalString;
+	document.getElementById('msgHref2').href= finalString; 
+}
+
+function changeTextA() {
+	var i = 0;
+	var date_i = formatDate(document.getElementById("date".concat(i.toString())).value);
+	
+	var text = 'Spark A, covid-spark.info<br>Mislim, da sva bila v zadnjem casu v stiku.<br>Sporocam ti, da sem morda od '+date_i+' asimptomaticni prenasalec virusa SARS-CoV-2';
+
+	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
+	
+	splitArray = text.split("<br>");
+	
+	var finalString = "sms:?body=";
+    for ( i = 0; i < splitArray.length; i++) {
+        finalString += splitArray[i]+ "%0a"; 
+        }
+	
+	document.getElementById('msgText').innerHTML=text;
+	document.getElementById('msgHref').href= finalString;
+	document.getElementById('msgHref2').href= finalString; 
+}
+
+var R_scene1_out = new Array("da sem bil v stiku z vec osebami, ki so se po ", "da sem bil ");
+var R_scene2_out = new Array(" izkazale za simptomaticne ali pozitivne na SARS-CoV-2", " na dogodku supersirjenja virusa SARS-CoV-2");
+
+function changeTextR() {
+	var text = 'Spark R, covid-spark.info<br>Mislim, da sva bila v zadnjem casu v stiku';
+	
+	flag = true;
+	for (i=0;i<R_scene1_out.length;i++) {
+		var checkBox = document.getElementById("simpt".concat(i.toString()));
+        var simpt1_i = R_scene1_out[i];
+		var simpt2_i = R_scene2_out[i];
+		var date_i = formatDate(document.getElementById("date".concat(i.toString())).value);
+		
+		if (checkBox.checked == true){
+			if (flag){
+				text = text.concat(".<br>Sporocam ti, ");
+				flag = false;
+			} else {
+			    text = text.concat(" in ");
+			}
+			text = text.concat(simpt1_i+date_i+simpt2_i);
+		}
+	}
+	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
+	
+	splitArray = text.split("<br>");
+	
+	var finalString = "sms:?body=";
+    for ( i = 0; i < splitArray.length; i++) {
+        finalString += splitArray[i]+ "%0a"; 
+        }
+	
+	document.getElementById('msgText').innerHTML=text;
+	document.getElementById('msgHref').href= finalString;
+	document.getElementById('msgHref2').href= finalString; 
 }
