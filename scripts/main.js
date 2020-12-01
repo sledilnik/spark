4395d6015745197c438665416dba2c9e690c1e91
Luka M <50453817+luka-medic@users.noreply.github.com>
2020-12-01 13:00:33 +0100

    čšž & proofreading

--- scripts/main.js
+++ scripts/main.js
@@ -61,10 +61,25 @@ function AdujstTxtGender(text, word, wordm, wordf) {
 	return text;
 }
 
-var simptoms_out = new Array("Vrocina", "Suh kaselj", "Utrujenost", "Bolecine", "Bolece zrelo", "Driska", "Konjunktivitis", "Glavobol", "Izguba okusa/vonja", "Srbecica/razbarvanost prstov", "Tezave pri dihanju/izguba sape", "Bolecina/pritisk v prsih", "Izguba zmožnosti govora/premikanja");
+function AdujstTxtSumnik(text, s_old, s_new) {
+	var checkBoxSumniki = document.getElementById("sumniki");
+	
+	splitArrayGender = text.split(s_old);
+	var finalStringG = splitArrayGender[0];
+	if (checkBoxSumniki.checked == false) {
+		for ( i = 1; i < splitArrayGender.length; i++) {
+			finalStringG += s_new+splitArrayGender[i]; 
+        }
+		return finalStringG;
+	} else {
+		return text;
+	}
+}
+
+var simptoms_out = new Array("Vročina", "Suh kašelj", "Utrujenost", "Bolečine v mišicah/sklepih", "Boleče žrelo", "Driska", "Konjunktivitis", "Glavobol", "Izguba okusa/vonja", "Srbečica/razbarvanost prstov", "Težave pri dihanju/izguba sape", "Bolečina/pritisk v prsih", "Izguba zmožnosti govora/premikanja");
 
 function changeTextS() {
-	var text = 'Spark S, covid-spark.info<br>Mislim, da sva bila v zadnjem casu v stiku';
+	var text = 'Spark S, covid-spark.info<br>Mislim, da sva bila v zadnjem času v stiku';
 	
 	flag = true;
 	flag2 = false;
@@ -75,7 +90,7 @@ function changeTextS() {
 		
 		if (checkBox.checked == true){
 			if (flag){
-				text = text.concat(".<br>Sporocam ti, da imam simptome, znacilne za COVID-19: ");
+				text = text.concat(".<br>Sporočam ti, da imam simptome, značilne za COVID-19: ");
 				flag = false;
 				flag2 = true;
 			} else {
@@ -85,13 +100,17 @@ function changeTextS() {
 		}
 	}
 	if (flag2) {
-		text = text.concat(".<br> Kuzen/Kuzna sem najverjetneje bil/a 2 dneva pred pricetkom prvih simptomov");
+		text = text.concat(".<br> Kužen/Kužna sem najverjetneje bil/a 2 dneva pred pričetkom prvih simptomov");
 	}
 	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
 	
 	text = AdujstTxtGender(text, "/a", "", "a");
-	text = AdujstTxtGender(text, "Kuzen/Kuzna", "Kuzen", "Kuzna");
-	text = AdujstTxtGender(text, "kuzen/kuzna", "kuzen", "kuzna");
+	text = AdujstTxtGender(text, "Kužen/Kužna", "Kužen", "Kužna");
+	text = AdujstTxtGender(text, "kužen/kužna", "kužen", "kužna");
+	
+	text = AdujstTxtSumnik(text, "č", "c");
+	text = AdujstTxtSumnik(text, "š", "s");
+	text = AdujstTxtSumnik(text, "ž", "z");
 	
 	splitArray = text.split("<br>");
 	
@@ -105,8 +124,8 @@ function changeTextS() {
 	document.getElementById('msgHref2').href= finalString; 
 }
 
-var P_scene1_out = new Array("Prvi simptom sem imel/a ", "Nimam simptomov, a mislim, da sem se okuzil/a ", "Nimam simptomov in ne vem, kdaj sem se okuzil/a");
-var P_scene2_out = new Array(", kuzen/kuzna pa bi lahko bil/a ze 2 dneva prej", ", 3 dni po tem pa bi lahko bil/a kuzen/kuzna", "");
+var P_scene1_out = new Array("Prvi simptom sem imel/a ", "Nimam simptomov, a mislim, da sem se okužil/a ", "Nimam simptomov in ne vem, kdaj sem se okužil/a");
+var P_scene2_out = new Array(", kužen/kužna pa bi lahko bil/a že 2 dneva prej", ", 3 dni po tem pa bi lahko bil/a kužen/kužna", "");
 
 function changeTextP() {
 	var text = 'Spark P, covid-spark.info<br>Imam pozitiven test na SARS-CoV-2';
@@ -137,8 +156,12 @@ function changeTextP() {
 	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
 	
 	text = AdujstTxtGender(text, "/a", "", "a");
-	text = AdujstTxtGender(text, "Kuzen/Kuzna", "Kuzen", "Kuzna");
-	text = AdujstTxtGender(text, "kuzen/kuzna", "kuzen", "kuzna");
+	text = AdujstTxtGender(text, "Kužen/Kužna", "Kužen", "Kužna");
+	text = AdujstTxtGender(text, "kužen/kužna", "kužen", "kužna");
+	
+	text = AdujstTxtSumnik(text, "č", "c");
+	text = AdujstTxtSumnik(text, "š", "s");
+	text = AdujstTxtSumnik(text, "ž", "z");
 	
 	splitArray = text.split("<br>");
 	
@@ -152,7 +175,7 @@ function changeTextP() {
 	document.getElementById('msgHref2').href= finalString; 
 }
 
-var N_scene_out = new Array("Imam negativen test na SARS-CoV-2.", "Moj potencialni vir okuzbe se je izkazal za negativno testiranega.");
+var N_scene_out = new Array("Imam negativen test na SARS-CoV-2.", "Moj potencialni vir okužbe se je izkazal za negativno testiranega.");
 
 function changeTextN() {
 	var text = 'Spark N, covid-spark.info<br>';
@@ -172,6 +195,10 @@ function changeTextN() {
 		}
 	}
 	
+	text = AdujstTxtSumnik(text, "č", "c");
+	text = AdujstTxtSumnik(text, "š", "s");
+	text = AdujstTxtSumnik(text, "ž", "z");
+	
 	splitArray = text.split("<br>");
 	
 	var finalString = "sms:?&body=";
@@ -181,19 +208,23 @@ function changeTextN() {
 	
 	document.getElementById('msgText').innerHTML=text;
 	document.getElementById('msgHref').href= finalString;
-	document.getElementById('msgHref2').href= finalString; 
+	document.getElementById('msgHref2').href= finalString;
 }
 
 function changeTextA() {
 	var i = 0;
 	var date_i = formatDate(document.getElementById("date".concat(i.toString())).value);
 	
-	var text = 'Spark A, covid-spark.info<br>Mislim, da sva bila v zadnjem casu v stiku.<br>Sporocam ti, da sem bil/a '+date_i+' v stiku z moznim superprenasalcem, kar pa pomeni, da bi lahko jaz postal/a 3 dni kasneje asimptomaticni prenasalec virusa';
+	var text = 'Spark A, covid-spark.info<br>Mislim, da sva bila v zadnjem času v stiku.<br>Sporočam ti, da sem bil/a '+date_i+' v stiku z možnim superprenašalcem, kar pa pomeni, da bi lahko jaz postal/a 3 dni kasneje asimptomatični prenašalec virusa';
 	
 	text = AdujstTxtGender(text, "/a", "", "a");
-	text = AdujstTxtGender(text, "Kuzen/Kuzna", "Kuzen", "Kuzna");
-	text = AdujstTxtGender(text, "kuzen/kuzna", "kuzen", "kuzna");
-
+	text = AdujstTxtGender(text, "Kužen/Kužna", "Kužen", "Kužna");
+	text = AdujstTxtGender(text, "kužen/kužna", "kužen", "kužna");
+	
+	text = AdujstTxtSumnik(text, "č", "c");
+	text = AdujstTxtSumnik(text, "š", "s");
+	text = AdujstTxtSumnik(text, "ž", "z");
+	
 	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
 	
 	splitArray = text.split("<br>");
@@ -208,11 +239,11 @@ function changeTextA() {
 	document.getElementById('msgHref2').href= finalString; 
 }
 
-var R_scene1_out = new Array("da sem bil/a v stiku z vec osebami, ki so se po ", "da sem bil/a ");
-var R_scene2_out = new Array(" izkazale za simptomaticne ali pozitivne na SARS-CoV-2, kar pomeni, da bi lahko bil/a jaz njihov vir okuzbe in kuzen/kuzna ze 5 dni prej", " na dogodku supersirjenja virusa SARS-CoV-2, kar pa pomeni, da bi lahko 3 dni po dogodku postal/a kuzen/kuzna");
+var R_scene1_out = new Array("da sem bil/a v stiku z več osebami, ki so se po ", "da sem bil/a ");
+var R_scene2_out = new Array(" izkazale za simptomatične ali pozitivne na SARS-CoV-2, kar pomeni, da bi lahko bil/a jaz njihov vir okužbe in kužen/kužna že 5 dni prej", " na dogodku superširjenja virusa SARS-CoV-2, kar pa pomeni, da bi lahko 3 dni po dogodku postal/a kužen/kužna");
 
 function changeTextR() {
-	var text = 'Spark R, covid-spark.info<br>Mislim, da sva bila v zadnjem casu v stiku';
+	var text = 'Spark R, covid-spark.info<br>Mislim, da sva bila v zadnjem času v stiku';
 	
 	flag = true;
 	for (i=0;i<R_scene1_out.length;i++) {
@@ -223,7 +254,7 @@ function changeTextR() {
 		
 		if (checkBox.checked == true){
 			if (flag){
-				text = text.concat(".<br>Sporocam ti, ");
+				text = text.concat(".<br>Sporočam ti, ");
 				flag = false;
 			} else {
 			    text = text.concat(", in ");
@@ -235,8 +266,12 @@ function changeTextR() {
 	text = text.concat(".<br>Preveri, kaj to pomeni zate: https://covid-spark.info");
 	
 	text = AdujstTxtGender(text, "/a", "", "a");
-	text = AdujstTxtGender(text, "Kuzen/Kuzna", "Kuzen", "Kuzna");
-	text = AdujstTxtGender(text, "kuzen/kuzna", "kuzen", "kuzna");
+	text = AdujstTxtGender(text, "Kužen/Kužna", "Kužen", "Kužna");
+	text = AdujstTxtGender(text, "kužen/kužna", "kužen", "kužna");
+	
+	text = AdujstTxtSumnik(text, "č", "c");
+	text = AdujstTxtSumnik(text, "š", "s");
+	text = AdujstTxtSumnik(text, "ž", "z");
 	
 	splitArray = text.split("<br>");
 	
@@ -250,3 +285,21 @@ function changeTextR() {
 	document.getElementById('msgHref2').href= finalString; 
 }
 
+function changeTextShare() {
+	var text = 'Obišči spletni portal https://covid-spark.info. Deli naprej, da omejimo virus.';
+	
+	text = AdujstTxtSumnik(text, "č", "c");
+	text = AdujstTxtSumnik(text, "š", "s");
+	text = AdujstTxtSumnik(text, "ž", "z");
+	
+	splitArray = text.split("<br>");
+	
+	var finalString = "sms:?&body=";
+    for ( i = 0; i < splitArray.length; i++) {
+        finalString += splitArray[i]+ "%0a"; 
+        }
+	
+	document.getElementById('msgText').innerHTML=text;
+	document.getElementById('msgHref').href= finalString;
+	document.getElementById('msgHref2').href= finalString;
+}
